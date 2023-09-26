414829a3c84d3a75bc1a95fb9691b8fb8c441a93
Qkrwnsgus0522 <134922079+Qkrwnsgus0522@users.noreply.github.com>
2023-09-26 17:10:00 +0900

    Add files via upload

---
+++ day93/src/main/java/com/jun/app/InterfaceBoardService.java
@@ -0,0 +1,11 @@
+package com.jun.app;
+
+import java.util.List;
+
+public interface InterfaceBoardService {
+	List<BoardDTO> selectAll(BoardDTO bDTO);
+	BoardDTO selectOne(BoardDTO bDTO);
+	boolean insert(BoardDTO bDTO);
+	boolean update(BoardDTO bDTO);
+	boolean delete(BoardDTO bDTO);
+}
---
+++ day93/src/main/java/com/jun/app/InterfaceMemberService.java
@@ -0,0 +1,11 @@
+package com.jun.app;
+
+import java.util.List;
+
+public interface InterfaceMemberService {
+	List<MemberDTO> selectAll(MemberDTO mDTO);
+	MemberDTO selectOne(MemberDTO mDTO);
+	boolean insert(MemberDTO mDTO);
+	boolean update(MemberDTO mDTO);
+	boolean delete(MemberDTO mDTO);
+}
